116b0a24321391f31b42ed24228e859d9ac7173e
Thomas Huguerre <huguerre.thomas@gmail.com>
2020-11-11 15:00:51 +0100

    implementing from and to day limits

--- baseball-japan-npb-crawler.py
+++ baseball-japan-npb-crawler.py
@@ -7,10 +7,14 @@ import logging
 URL_Farm_Leagues = "https://npb.jp/bis/eng/<YEAR>/calendar/index_farm_<MONTH>.html"
 URL_Regular_Season = "https://npb.jp/bis/eng/<YEAR>/calendar/index_<MONTH>.html"
 
-def retrieve_month_results(year_to_get, month_to_get, league_to_get):
+def retrieve_month_results(year_to_get, month_to_get, league_to_get, from_day, to_day):
 
     if len(month_to_get) == 1:
         month_to_get = "0" + month_to_get
+
+    logging.debug("retrieving '" + league_to_get + "' results for " + year_to_get + "-" + month_to_get)
+    logging.debug("from_day : " + str(from_day))
+    logging.debug("to_day : " + str(to_day))
 
     if league_to_get == 'Farm Leagues':
         url = URL_Farm_Leagues.replace('<YEAR>', year_to_get).replace('<MONTH>', month_to_get)
@@ -29,10 +33,20 @@ def retrieve_month_results(year_to_get, month_to_get, league_to_get):
     allmatches = allmatches_regex.findall(wholepage.text)
 
     for onematch in allmatches:
-        day = day_regex.search(onematch).group(1)
-        result = matchresult_regex.search(onematch).group(1)
+        match_day = day_regex.search(onematch).group(1)
+        match_result = matchresult_regex.search(onematch).group(1)
 
-        print(day[0:4] + "-" + day[4:6] + "-" + day[6:9] + "\t\t" + league_to_get + "\t\t\t " + result)
+        print_match = True
+        
+        if from_day > 0 and from_day > int(match_day[6:9]):
+            print_match = False
+
+        if to_day > 0 and to_day < int(match_day[6:9]):
+            print_match = False
+
+        if print_match:
+            print(match_day[0:4] + "-" + match_day[4:6] + "-" + match_day[6:9] + "\t\t" + league_to_get + "\t\t\t " + match_result)
+
 
 def retrieve_results(start_year, start_month, start_day, to_year, to_month, to_day, league_to_get):
 
@@ -44,7 +58,16 @@ def retrieve_results(start_year, start_month, start_day, to_year, to_month, to_d
     increment_month = True
     while increment_month:
         
-        retrieve_month_results(str(current_year), str(current_month), league_to_get)
+        local_from_day = 0
+        local_to_day = 0
+
+        if current_year == int(start_year) and current_month == int(start_month):
+            local_from_day = int(start_day)
+
+        if current_year == int(to_year) and current_month == int(to_month):
+            local_to_day = int(to_day)
+
+        retrieve_month_results(str(current_year), str(current_month), league_to_get, local_from_day, local_to_day)
 
         if current_year < int(to_year):
             current_month += 1
@@ -71,7 +94,7 @@ def printDocumentation():
 #
 if __name__ == '__main__':
     
-    logging.getLogger().setLevel(logging.WARN)
+    logging.getLogger().setLevel(logging.INFO)
 
     today = date.today()
 
@@ -91,9 +114,9 @@ if __name__ == '__main__':
             start_day = args[11:13]
 
         elif args.startswith("to:"):
-            start_year = args[3:7]
-            start_month = args[7:9]
-            start_day = args[9:11]
+            to_year = args[3:7]
+            to_month = args[7:9]
+            to_day = args[9:11]
 
         elif args == '-h' or args == "-help" or args == "--h" or args == "--help":
             printDocumentation()

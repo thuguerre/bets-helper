4bda8e1fefc3ba49768cf131707b902fc8c4e6a2
Thomas Huguerre <huguerre.thomas@gmail.com>
2021-06-11 11:38:32 +0200

    Update baseball-japan-npb-crawler.py

--- baseball-japan-npb-crawler.py
+++ baseball-japan-npb-crawler.py
@@ -1,11 +1,22 @@
 import requests
 import re
 import sys
+import os
 from datetime import date, timedelta
 import logging
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 URL_Farm_Leagues = "https://npb.jp/bis/eng/<YEAR>/calendar/index_farm_<MONTH>.html"
 URL_Regular_Season = "https://npb.jp/bis/eng/<YEAR>/calendar/index_<MONTH>.html"
+
+SPREADSHEET_NAME = "Suivi paris"
+SPREADSHEET_INDEX = 6               # index of 'Baseball Japan RAW', starting from 0
+
+
+def next_available_row(worksheet):
+    str_list = list(filter(None, worksheet.col_values(1)))
+    return str(len(str_list)+1)
 
 def retrieve_month_results(year_to_get, month_to_get, league_to_get, from_day, to_day):
 
@@ -32,6 +43,8 @@ def retrieve_month_results(year_to_get, month_to_get, league_to_get, from_day, t
 
     allmatches = allmatches_regex.findall(wholepage.text)
 
+    month_results = []
+
     for onematch in allmatches:
         match_day = day_regex.search(onematch).group(1)
         match_result = matchresult_regex.search(onematch).group(1)
@@ -46,6 +59,9 @@ def retrieve_month_results(year_to_get, month_to_get, league_to_get, from_day, t
 
         if print_match:
             print(match_day[0:4] + "-" + match_day[4:6] + "-" + match_day[6:9] + "\t\t" + league_to_get + "\t\t\t " + match_result)
+            month_results.append([match_day[6:9] + "/" + match_day[4:6] + "/" + match_day[0:4], league_to_get, match_result])
+    
+    return month_results
 
 
 def retrieve_results(start_year, start_month, start_day, to_year, to_month, to_day, league_to_get):
@@ -54,6 +70,8 @@ def retrieve_results(start_year, start_month, start_day, to_year, to_month, to_d
 
     current_year = int(start_year)
     current_month = int(start_month)
+
+    results = []
 
     increment_month = True
     while increment_month:
@@ -67,7 +85,9 @@ def retrieve_results(start_year, start_month, start_day, to_year, to_month, to_d
         if current_year == int(to_year) and current_month == int(to_month):
             local_to_day = int(to_day)
 
-        retrieve_month_results(str(current_year), str(current_month), league_to_get, local_from_day, local_to_day)
+        month_results = retrieve_month_results(str(current_year), str(current_month), league_to_get, local_from_day, local_to_day)
+        for month_result in month_results:
+            results.append(month_result)
 
         if current_year < int(to_year):
             current_month += 1
@@ -81,6 +101,64 @@ def retrieve_results(start_year, start_month, start_day, to_year, to_month, to_d
         else:
             raise "Should not be in this case"
 
+    return results
+
+def upload_results(results):
+
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    #creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    
+    credentials = {
+        "type": "service_account",
+        "project_id": os.getenv('SA_PROJECT_ID'),
+        "private_key_id": os.getenv("SA_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("SA_PRIVATE_KEY"),
+        "client_email": os.getenv("SA_CLIENT_EMAIL"),
+        "client_id": os.getenv("SA_CLIENT_ID"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.getenv("SA_CLIENT_X509_CERT_URL")
+    }
+    creds = gspread.service_account_from_dict(credentials)
+    client = gspread.authorize(creds)
+
+    jpn_raw_sheet = client.open(SPREADSHEET_NAME).get_worksheet(SPREADSHEET_INDEX)
+
+    for result in results:
+        next_row = next_available_row(jpn_raw_sheet)
+        jpn_raw_sheet.insert_row(['temp'], int(next_row))
+        jpn_raw_sheet.update(
+            'A' + next_row + ':U' + next_row,
+            [
+                [
+                    result[0],
+                    '=WEEKDAY(A' + next_row + ';2)',
+                    result[1],
+                    '=IF(G'+next_row+'<K'+next_row+';G'+next_row+';K'+next_row+')',
+                    '=IF(G'+next_row+'>K'+next_row+';G'+next_row+';K'+next_row+')',
+                    result[2],
+                    '=SPLIT(F'+next_row+';" ")',
+                    '',
+                    '',
+                    '',
+                    '',
+                    '=IF(H'+next_row+'="*";0;1)',
+                    '=IF(L'+next_row+'=0;"*";IF(H'+next_row+'>J'+next_row+';G'+next_row+';K'+next_row+'))',
+                    '=IF(L'+next_row+'=1;$H'+next_row+'+$J'+next_row+';"")',
+                    '=IF(AND(L'+next_row+'=1;N'+next_row+'>3,5);1;0)',
+                    '=IF(AND(L'+next_row+'=1;N'+next_row+'>4,5);1;0)',
+                    '=IF(AND(L'+next_row+'=1;N'+next_row+'>5,5);1;0)',
+                    '=IF(AND(L'+next_row+'=1;N'+next_row+'>6,5);1;0)',
+                    '=IF(AND(L'+next_row+'=1;N'+next_row+'>7,5);1;0)',
+                    '=IF(AND(L'+next_row+'=1;N'+next_row+'>8,5);1;0)',
+                    '=IF(AND(L'+next_row+'=1;N'+next_row+'>9,5);1;0)'
+                ]
+            ],
+            raw=False
+        )
+
 #
 # Documentation Printing Method
 #
@@ -89,6 +167,7 @@ def printDocumentation():
     print("args 'from:YYYYMMDD'. If not set, default value is today.")
     print("args 'to:YYYYMMDD'. If not set, default value is today.")
     print("args 'yesterday': set FROM and TO date limits at yesterday's date.")
+    print("args 'upload', set to yes or no, to upload to spreadsheet.")
 
 #
 # Main Function
@@ -106,6 +185,8 @@ if __name__ == '__main__':
     to_year = today.strftime("%Y")
     to_month = today.strftime("%m")
     to_day = today.strftime("%d")
+
+    upload = True
 
     for args in sys.argv:
 
@@ -130,6 +211,12 @@ if __name__ == '__main__':
             to_month = yesterday.strftime("%m")
             to_day = yesterday.strftime("%d")
 
+        elif args.lower() == "upload:yes":
+            upload = True
+
+        elif args.lower() == "upload:no":
+            upload = False
+
         elif args == '-h' or args == "-help" or args == "--h" or args == "--help":
             printDocumentation()
             sys.exit()
@@ -147,6 +234,7 @@ if __name__ == '__main__':
         logging.error("FROM day cannnot be greater than TO day")
         sys.exit()
 
-    retrieve_results(start_year, start_month, start_day, to_year, to_month, to_day, 'Regular Season')
-    
+    results = retrieve_results(start_year, start_month, start_day, to_year, to_month, to_day, 'Regular Season')
 
+    if(upload):
+        upload_results(results)

5f0f0fcf59f9820e0217a21e9fc1cb759e781dc0
Thomas Huguerre <huguerre.thomas@gmail.com>
2021-06-11 18:03:14 +0200

    Update baseball-japan-npb-crawler.py

--- baseball-japan-npb-crawler.py
+++ baseball-japan-npb-crawler.py
@@ -132,15 +132,15 @@ def upload_results(results):
     
     credentials = {
         "type": "service_account",
-        "project_id": os.environ('SA_PROJECT_ID'),
-        "private_key_id": os.environ("SA_PRIVATE_KEY_ID"),
-        "private_key": os.environ("SA_PRIVATE_KEY"),
-        "client_email": os.environ("SA_CLIENT_EMAIL"),
-        "client_id": os.environ("SA_CLIENT_ID"),
+        "project_id": os.environ["SA_PROJECT_ID"],
+        "private_key_id": os.environ["SA_PRIVATE_KEY_ID"],
+        "private_key": os.environ["SA_PRIVATE_KEY"],
+        "client_email": os.environ["SA_CLIENT_EMAIL"],
+        "client_id": os.environ["SA_CLIENT_ID"],
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.environ("SA_CLIENT_X509_CERT_URL")
+        "client_x509_cert_url": os.environ["SA_CLIENT_X509_CERT_URL"]
     }
     creds = gspread.service_account_from_dict(credentials)
     client = gspread.authorize(creds)

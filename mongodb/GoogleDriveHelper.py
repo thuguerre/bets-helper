76778f55fb81d3b16ac46fa350b44011c0de272f
Thomas Huguerre <huguerre.thomas@gmail.com>
2021-06-23 22:26:42 +0200

    cleaning code to prepare new GDrive function

---
+++ mongodb/GoogleDriveHelper.py
@@ -0,0 +1,37 @@
+import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class GoogleDriveHelper:
+
+    drive = None
+
+    def __init__(self):
+        # use creds to create a client to interact with the Google Drive API
+        credentials = {
+            "type": "service_account",
+            "project_id": os.environ["SA_PROJECT_ID"],
+            "private_key_id": os.environ["SA_PRIVATE_KEY_ID"],
+            "private_key": os.environ["SA_PRIVATE_KEY"],
+            "client_email": os.environ["SA_CLIENT_EMAIL"],
+            "client_id": os.environ["SA_CLIENT_ID"],
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": os.environ["SA_CLIENT_X509_CERT_URL"]
+        }
+
+        # connecting to Google Drive
+        gauth = GoogleAuth()
+        scope = ['https://www.googleapis.com/auth/drive']
+        gauth.credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials, scope)
+        self.drive = GoogleDrive(gauth)
+
+    def upload_files(self, files):
+
+        for upload_file in files:
+            gfile = self.drive.CreateFile()
+            gfile.SetContentFile(upload_file)
+            gfile.Upload()
--- mongodb/main_dump_db.py
+++ mongodb/main_dump_db.py
@@ -3,9 +3,7 @@ from LocalExecHelper import LocalExecHelper
 import logging
 import sys
 import os
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from oauth2client.service_account import ServiceAccountCredentials
+from GoogleDriveHelper import GoogleDriveHelper
 
 
 #
@@ -32,36 +30,14 @@ if __name__ == '__main__':
     files = mongodb.dumpDB()
     logging.info("dumped.")
 
-    # use creds to create a client to interact with the Google Drive API
-    credentials = {
-        "type": "service_account",
-        "project_id": os.environ["SA_PROJECT_ID"],
-        "private_key_id": os.environ["SA_PRIVATE_KEY_ID"],
-        "private_key": os.environ["SA_PRIVATE_KEY"],
-        "client_email": os.environ["SA_CLIENT_EMAIL"],
-        "client_id": os.environ["SA_CLIENT_ID"],
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.environ["SA_CLIENT_X509_CERT_URL"]
-    }
-
-    # connecting to Google Drive
-    gauth = GoogleAuth()
-    scope = ['https://www.googleapis.com/auth/drive']
-    gauth.credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials, scope)
-    drive = GoogleDrive(gauth)
-
-    for upload_file in files:
-	    gfile = drive.CreateFile()
-	    gfile.SetContentFile(upload_file)
-	    gfile.Upload()
-
+    # uploading dumped files to Google Drive
+    drive = GoogleDriveHelper()
+    drive.upload_files(files)
+    logging.info("files uploaded to GDrive")
+    
     # deleting files if asked
     if delete_files:
         for delete_file in files:
             os.remove(delete_file)
 
         logging.info("files deleted")
-
-    logging.info("mongodb dumped and uploaded")

dc98d4c97c47058f42d1e948a71d575e7bce9434
Thomas Huguerre <huguerre.thomas@gmail.com>
2021-06-23 20:25:46 +0200

    deleting dump files if asked

--- mongodb/main_dump_db.py
+++ mongodb/main_dump_db.py
@@ -2,6 +2,8 @@ from BetsMongoDB import BetsMongoDB
 from LocalExecHelper import LocalExecHelper
 import logging
 import sys
+import os
+
 
 #
 # Main Function
@@ -10,11 +12,22 @@ if __name__ == '__main__':
 
     logging.getLogger().setLevel(logging.INFO)
 
+    delete_files = False
+
     # loading script argument
     for args in sys.argv:
 
         if args == "local_exec":
             LocalExecHelper()
+        
+        elif args == "delete_files:yes":
+            delete_files = True
 
+    # dumping MongoDB to files
     mongodb = BetsMongoDB()
-    mongodb.dumpDB()
+    files = mongodb.dumpDB()
+
+    # deleting files if asked
+    if delete_files:
+        for file in files:
+            os.remove(file)

9ecc927aaee6523ff7f8a1df171d6b7425f2ec24
Thomas Huguerre <huguerre.thomas@gmail.com>
2021-06-29 22:09:03 +0200

    croning retrieving odds

--- main_retrieve_odds.py
+++ main_retrieve_odds.py
@@ -1,11 +1,7 @@
-from WinamaxCrawler import WinamaxCrawler
+from baseball_japan.WinamaxCrawler import WinamaxCrawler
 import logging
 import os
 import sys
-
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir)
 from mongodb.BetsMongoDB import BetsMongoDB
 from LocalExecHelper import LocalExecHelper
 
@@ -17,12 +13,16 @@ if __name__ == '__main__':
 
     logging.getLogger().setLevel(logging.INFO)
 
+    try:
+        os.environ["MONGODB_NAME"]
+    except KeyError:
+        LocalExecHelper()
+
+    # retrieving odds from Winamax
     crawler = WinamaxCrawler()
     matches = crawler.retrieve_odds()
-
-    LocalExecHelper()
+        
+    # inserting data in MongoDB
     mongodb = BetsMongoDB()
     for match in matches:
         mongodb.insertMatchOrAppendOdds(match)
-
-
